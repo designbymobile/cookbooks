48f53287eb215fffd7e7b7076ff150012b1eca9c
LEO-DBM <leo@miistory.com>
2014-11-24 17:26:59 +1300

    update cookbook with aws keys

--- deploy/recipes/appsetup.rb
+++ deploy/recipes/appsetup.rb
@@ -15,9 +15,18 @@ node[:deploy].each do |app_name, deploy|
       :user =>     (deploy[:database][:username] rescue nil),
       :password => (deploy[:database][:password] rescue nil),
       :db =>       (deploy[:database][:database] rescue nil),
+      
       :smtphost =>  (deploy[:smtp][:host] rescue nil),
       :smtpuser => (deploy[:smtp][:username] rescue nil),
-      :smtppassword => (deploy[:smtp][:password] rescue nil) )
+      :smtppassword => (deploy[:smtp][:password] rescue nil),
+      
+      :aws_access_key => (deploy[:aws][:access_key] rescue nil),
+      :aws_secret_key => (deploy[:aws][:secret_key] rescue nil),
+      :client_buket => (deploy[:aws][:client_buket] rescue nil),
+      :cache_cluster_host => (deploy[:aws][:cache_cluster_host] rescue nil)
+      
+      )
+      
    
   end
 end
